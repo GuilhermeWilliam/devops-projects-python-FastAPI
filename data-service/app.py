51420b1ca85e486eeb71eedda02b201c345ae2f9
guilhermeJ <gjoao@Guilhermes-MacBook-Pro.local>
2026-04-16 12:21:14 +0100

    add monitoring (prometheus + grafana)

--- data-service/app.py
+++ data-service/app.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 
 app = FastAPI()
+REQUEST_COUNT = Counter(
+    "data_service_requests_total",
+    "Total requests to data service"
+)
 
 @app.get("/data")
 def get_data():
+    REQUEST_COUNT.inc()
     return {
         "message": "Hello from data-service",
         "items": ["apple", "banana", "orange"]
@@ -11,7 +18,12 @@ def get_data():
 
 @app.get("/author")
 def get_author():
+    REQUEST_COUNT.inc()
     return {
         "name": "Guilherme L. Joao",
         "title": "MsC Telecom, Senior QA Engineer"
     }
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
--- gateway-service/app.py
+++ gateway-service/app.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 import requests
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 
 app = FastAPI()
-
+REQUEST_COUNT = Counter(
+    "gateway_requests_total",
+    "Total requests to gateway"
+)
 DATA_SERVICE_URL = "http://data-service:3001/data"
 AUTHOR_SERVICE_URL = "http://data-service:3001/author"
 
 @app.get("/")
 def root():
+    REQUEST_COUNT.inc()
     try:
         data = requests.get(DATA_SERVICE_URL, timeout=3).json()
     except requests.exceptions.RequestException:
@@ -23,3 +29,7 @@ def root():
         "data_service_response": data,
         "author_service_response": author
     }
+
+@app.get("/metrics")
+def metrics():
+    return Response(generate_latest(), media_type="text/plain")
